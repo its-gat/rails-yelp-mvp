a19e881db5e3387d53c94d3283fad661e03fb61a
Andjiane Raveloson <andrianina_raveloson@hotmail.fr>
2024-08-07 15:56:17 +0200

    Completed Rails-Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Dropping db'
+Restaurant.destroy_all
+puts 'Db dropped'
+
+puts 'creating restaurants'
+Restaurant.create!(name: 'joli', address: 'Toulouse', category: 'chinese')
+Restaurant.create!(name: 'moche', address: 'Paris', category: 'french')
+Restaurant.create!(name: 'grand', address: 'Bordeaux', category: 'italian')
+Restaurant.create!(name: 'gros', address: 'Lyon', category: 'japanese')
+Restaurant.create!(name: 'costaud', address: 'Montpellier', category: 'belgian')
+puts 'restaurants created'
